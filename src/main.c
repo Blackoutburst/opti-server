9d3c493a4cdf44f40b61f804cc6119ce711b74af
Florian Chanson <f.chanson@barbablanka.com>
2025-01-28 10:18:31 +0100

    start message

--- src/main.c
+++ src/main.c
@@ -11,7 +11,7 @@ I32 main(I32 argc, I8** argv) {
     U8 renderDistance = 2;
     if (argc > 1) updateRenderDistance(&renderDistance, argv[1]);
 
-    printf("Hello world %i\n", renderDistance);
+    printf("Starting server with a max render distance of: %i\n", renderDistance);
     return 0;
 }
 
