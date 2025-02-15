49e42716ce29241467084350e55d66a08ba65b7c
Blackoutburst <blackoutburst@hotmail.com>
2025-02-15 23:28:47 +0100

    test

--- build-core-osx-dev.sh
+++ build-core-osx-dev.sh
@@ -1,4 +1,4 @@
-# sed -i 's/\r$//' src/*.c src/utils/*.c src/network/*.c src/world/*.c src/database/*.c src/sqlite/*.c build-osx.sh
+# sed -i 's/\r$//' core/src/*.c core/src/library/*.c core/src/utils/*.c core/src/network/*.c core/src/world/*.c core/src/database/*.c core/src/sqlite/*.c build-core-osx-dev.sh
 clang \
 core/src/*.c \
 core/src/library/*.c
