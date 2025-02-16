36c88f69a5af8086e03eb2355dfdc074ef71b6f5
Rafale25 <rafale25.dev@gmail.com>
2025-02-16 22:13:33 +0100

    added glm

--- worldgen/src/main.cpp
+++ worldgen/src/main.cpp
@@ -5,22 +5,55 @@
 #define CHUNK_SIZE (16)
 
 #include "FastNoise/FastNoise.h"
+
+#define GLM_SWIZZLE
+#include "glm/glm.hpp"
+
 #include "main.h"
 
-// float fract(vec2 v) {
-//     return x - floor(x)
+// float de(glm::vec3 p)
+// {
+//     glm::vec2 t = glm::fract(glm::vec2(p.x, p.z))-0.5f;
+//     p.x=t.x;
+//     p.z=t.y;
+//     float k=1.0f;
+//     float s=0.0f;
+//     for(int i =0;i++<9;)
+//       s=2./glm::clamp(glm::dot(p,p),0.1f,1.0f),
+//       p=glm::abs(p)*s-glm::vec3(0.5f, 3.0f, 0.5f),
+//       k*=s;
+//     return glm::length(p)/k-.001f;
+//   }
+
+// float de(glm::vec3 p){  //(distance bound doesn't hold)
+//     glm::vec3 v=glm::vec3(0,1.5,6.3);
+//     return glm::min(6.0f-glm::length((p-v).xy+glm::sin(p.yx())), glm::dot(glm::cos(p),glm::sin(p.yzx())))+glm::sin(glm::sin(p.z*3.5f)+v.z)*0.1f+1.0f;
 // }
 
-// float de(vec3 p){
-//     p.xz=fract(p.xz)-.5;
-//     float k=1.;
-//     float s=0.;
-//     for(int i=0;i++<9;)
-//       s=2./clamp(dot(p,p),.1,1.),
-//       p=abs(p)*s-vec3(.5,3,.5),
-//       k*=s;
-//     return length(p)/k-.001;
+
+// if ( de(glm::vec3(world_x, world_y - 200, world_z) * 0.01f) < 0.005f) {
+// float de(glm::vec3 p) {
+//     const glm::vec3 va = glm::vec3(  0.0,  0.57735,  0.0 );
+//     const glm::vec3 vb = glm::vec3(  0.0, -1.0,  1.15470 );
+//     const glm::vec3 vc = glm::vec3(  1.0, -1.0, -0.57735 );
+//     const glm::vec3 vd = glm::vec3( -1.0, -1.0, -0.57735 );
+//     float a = 0.0f;
+//     float s = 1.0f;
+//     float r = 1.0f;
+//     float dm;
+//     glm::vec3 v;
+//     for(int i=0; i<16; i++) {
+//       float d, t;
+//       d = glm::dot(p-va,p-va);              v=va; dm=d; t=0.0f;
+//       d = glm::dot(p-vb,p-vb); if( d < dm ) { v=vb; dm=d; t=1.0f; }
+//       d = glm::dot(p-vc,p-vc); if( d < dm ) { v=vc; dm=d; t=2.0f; }
+//       d = glm::dot(p-vd,p-vd); if( d < dm ) { v=vd; dm=d; t=3.0f; }
+//       p = v + 2.0f*(p - v); r*= 2.0f;
+//       a = t + 4.0f*a; s*= 4.0f;
+//     }
+//     return (glm::sqrt(dm)-1.0f)/r;
 //   }
+
 
 void genChunk(uint8_t* blocks, int32_t x, int32_t y, int32_t z) {
     auto fn = FastNoise::New<FastNoise::Perlin>();
@@ -34,7 +67,9 @@ void genChunk(uint8_t* blocks, int32_t x, int32_t y, int32_t z) {
         int i = dz * CHUNK_SIZE*CHUNK_SIZE + dy * CHUNK_SIZE + dx;
         int iXZ = dz * CHUNK_SIZE + dx;
 
+        int world_x = x + dx;
         int world_y = y + dy;
+        int world_z = z + dz;
 
         if (world_y >= (2 + hmap[iXZ]) * 30.0f) {
             blocks[i] = 0;
--- worldgen/xmake.lua
+++ worldgen/xmake.lua
@@ -3,11 +3,9 @@ add_rules("mode.debug", "mode.release")
 set_languages("c++20")
 
 package("fastnoise2")
-    -- set_kind("static")
     add_deps("cmake")
     set_sourcedir(path.join(os.scriptdir(), "lib/fastnoise2"))
     add_includedirs("include")
-    -- add_defines("FASTNOISE_STATIC_LIB")
 
     on_install(function (package)
         local configs = {}
@@ -15,24 +13,56 @@ package("fastnoise2")
         table.insert(configs, "-DBUILD_SHARED_LIBS=OFF")
         table.insert(configs, "-DFASTNOISE2_NOISETOOL=OFF")
         table.insert(configs, "-DFASTNOISE2_TESTS=OFF")
-        -- table.insert(configs, "-DBUILD_STATIC_LIBS=ON")
-        -- table.insert(configs, "-DFASTNOISE_STATIC_LIB=1")
 
         import("package.tools.cmake").install(package, configs)
     end)
 package_end()
 
+package("glm")
+    add_deps("cmake")
+    set_sourcedir(path.join(os.scriptdir(), "lib/glm"))
+
+    on_load(function (package)
+        if package:config("modules") then
+            package:config_set("header_only", false)
+            package:config_set("cxx_standard", "20")
+        elseif package:config("header_only") then
+            package:set("kind", "library", {headeronly = true})
+        else
+            package:add("deps", "cmake")
+        end
+    end)
+
+    on_install(function (package)
+        if package:config("header_only") then
+            os.cp("glm", package:installdir("include"))
+        else
+            io.replace("CMakeLists.txt", "NOT GLM_DISABLE_AUTO_DETECTION", "FALSE")
+            local configs = {"-DGLM_BUILD_TESTS=OFF"}
+            table.insert(configs, "-DBUILD_SHARED_LIBS=OFF")
+            table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
+            table.insert(configs, "-DCMAKE_CXX_STANDARD=20")
+            import("package.tools.cmake").install(package, configs)
+        end
+    end)
+package_end()
+
 add_requires("fastnoise2")
+add_requires("glm")
 
 target("worldgen")
     set_prefixname("") -- To remove the prefix lib of shared library
     set_kind("shared")
     add_defines("MYLIB_EXPORTS")
 
+    add_includedirs("include")
+
     -- add_cxflags("-W -Wall -Wextra -Wpedantic -Wno-unused-parameter")
     set_optimize("fastest")
     add_files("src/*.cpp")
+
     add_packages("fastnoise2")
+    add_packages("glm")
 
     after_build(function (target)
         print("Built target: %s %s", target:name(), target:targetfile())
