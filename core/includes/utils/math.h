ee162af4bc049cbbd0fd6876d2769c621c46b227
Rafale25 <rafale25.dev@gmail.com>
2025-02-18 08:04:13 +0100

    MIN MAX MIX macros

--- core/includes/utils/math.h
+++ core/includes/utils/math.h
@@ -5,5 +5,11 @@
 
 #define CUBE(x) (x * x * x)
 
+#define MIN(a, b) (a > b ? b : a)
+#define MAX(a, b) (a < b ? b : a)
+#define MIX(x, y, a) (x*(1.0-a)+y*a)
+
+
+
 U32 xyzToIndex(I32 x, I32 y, I32 z);
 void indexToXYZ(VECTORI* vector, I16 index);
